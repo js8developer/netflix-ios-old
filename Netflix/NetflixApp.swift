77e4316ea2fd6be7c0a3915e9acafddf431f5545
JasonSchneider8 <36863375+JasonSchneider8@users.noreply.github.com>
2022-07-14 01:13:00 -0400

    Initial Commit

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Jason Schneider on 7/14/22.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
